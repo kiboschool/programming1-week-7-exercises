ad5c033edd6b4625a6f5be0013f48e04848b9148
Rob Cobb <rwcobbjr@gmail.com>
2022-09-27 23:31:45 -0600

    add solution

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
-# TODO: Import a module to help you with files
+import os
 
 def show_line_counts():
-    # TODO: 
-    # get filename from the user and, optionally, a lookup value
-    # get the line count from "file_line_count" and print results
-    pass
+    filename = input("Please enter a file name or path for line counting: ")
+    lookup_value = input("Optional: Please enter a value for lookup - Press [Enter] for no value: ")
 
-def line_count(filename: str, lookup_value: str) -> int:
+    if lookup_value:
+        count = line_count(filename=filename, lookup_value=lookup_value)
+        if count == -1:
+            print("Error: Please enter a valid file name")
+        else:
+            print(f"Number of lines containing [{lookup_value}] is: {count}")
+    else:
+        count = line_count(filename=filename)
+        if count == -1:
+            print("Error: Please enter a valid file name")
+        else:
+            print(f"Number of lines in file: {count}")
+
+
+def line_count(filename: str, lookup_value=None):
     """
-    Counts the number of lines in file. 
+    Counts the number of lines in file.
     If a lookup value is present, only counts lines that contain that value
-    If the file does not exist, returns -1 
+    If the file does not exist, returns -1
     :param: filename: name of the file to count lines from
     :param: lookup_value: string value to lookup for within the file
     :return: int number of lines matching lookup value, or -1
     """
-    pass
-
-
+    count = 0
+    if os.path.isfile(filename):
+        with open(filename, 'r') as our_file:
+            data = our_file.readlines()
+        if lookup_value:
+            for line in data:
+                if lookup_value in line:
+                    count += 1
+            return count
+        else:
+            return len(data)
+    else:
+        return -1
 
 if __name__ == "__main__":
-    # TODO: Call function here
-    pass
+    show_line_counts()
