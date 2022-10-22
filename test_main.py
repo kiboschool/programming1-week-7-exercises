5747d82a6a8ba6c0a047a5bb16240297e34f466c
Muawiya Asali <muawiya.asali@gm.com>
2022-10-22 22:57:30 +0300

    Update unittests

--- test_main.py
+++ test_main.py
@@ -3,11 +3,11 @@ from main import *
 
 class TestCountFileLines(unittest.TestCase):
     def test_file_line_count(self):
-        self.assertEqual(line_count(filename='README.md', lookup_value=''), 35)
+        self.assertEqual(line_count(filename='README.md', lookup_value=''), 77)
 
     def test_file_line_count_with_optional_argument(self):
-        self.assertEqual(line_count(filename='README.md', lookup_value='Flow'), 1)
-        self.assertEqual(line_count(filename='README.md', lookup_value='Test'), 0)
+        self.assertEqual(line_count(filename='README.md', lookup_value='Validation'), 2)
+        self.assertEqual(line_count(filename='README.md', lookup_value='Testing'), 1)
         self.assertEqual(line_count(filename='README.md', lookup_value='-1'), 0)
 
     def test_file_line_count_wrong_file(self):
